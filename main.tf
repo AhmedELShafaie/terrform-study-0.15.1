035db75d822fd5470c6fb9b603e1ad90b6381cd4
Ahmed ElShafaie <ahmed.fathy@gmail.com>
2021-04-28 02:32:46 +0200

    Modify .gitignore to ignore any terraform results Add random_password resourse and sensitive param

--- main.tf
+++ main.tf
@@ -8,7 +8,18 @@ resource "random_string" "mystring" {
   upper = false
 }
 
+resource "random_password" "rand_password" {
+  length = 10
+  special = true
+ 
+}
 output "random_string" {
     value = random_string.mystring.result
   
 }
+
+output "rand_password_output" {
+  sensitive = true
+  value = random_password.rand_password.result
+}
+
