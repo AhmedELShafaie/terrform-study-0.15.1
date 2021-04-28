4b0244af271ac30804d26979786f169ac2244f9e
Ahmed ElShafaie <ahmed.fathy@gmail.com>
2021-04-28 03:34:01 +0200

    Add output.tf for output values Add nonsensitive funtion to unmask the sensitive password

--- main.tf
+++ main.tf
@@ -9,17 +9,16 @@ resource "random_string" "mystring" {
 }
 
 resource "random_password" "rand_password" {
-  length = 10
+  length = 20
   special = true
  
 }
-output "random_string" {
-    value = random_string.mystring.result
-  
-}
 
-output "rand_password_output" {
-  sensitive = true
-  value = random_password.rand_password.result
+
+resource "random_pet" "mypet" {
+    length = 10
+    prefix = "mypet"
+    separator = "-"
+  
 }
 
